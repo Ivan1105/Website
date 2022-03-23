df5ff1c20863423f80a304ddfd3586abce14c46b
Ivan1105 <1058267816@qq.com>
2022-03-23 10:41:02 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
+
+const app = createApp(App)
+app.use(router).mount('#app')
