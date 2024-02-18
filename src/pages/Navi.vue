4bb9b299c368a1b7560cbc00480202dd0af2717c
Ivan1105 <1058267816@qq.com>
2024-02-18 17:11:54 +0800

    导航页调整

--- src/pages/Navi.vue
+++ src/pages/Navi.vue
@@ -1,15 +1,9 @@
 <template>
-    <v-container class="navi-container">
-        <v-row>
-            <v-col v-for="item in navis" :key="item.title" cols="12" md="6" lg="4">
-                <v-card class="navi-card h-100" :title="item.title" :image="item.background" to="/login" min-height="200">
-                    <v-card-text>
-                        <p v-for="para in item.introduction" :key="para">{{ para }}</p>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+    <div class="navi-container">
+        <div class="child" v-for="item in navis" :key="item.title">
+            <img :src="item.background">
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -37,7 +31,7 @@ const navis = ref<any[]>([
     },
 ]);
 
-navis.value = new Array(20).fill({}).map(item => {
+navis.value = new Array(9).fill({}).map(item => {
     const introduction = new Array(((Math.random() * 3) | 0) + 1).fill('这是一段文本');
     return {
         title: 'TEST',
@@ -49,34 +43,109 @@ navis.value = new Array(20).fill({}).map(item => {
 
 <style lang="scss" scoped>
 .navi-container {
-    &::before {
-        content: "";
-        position: absolute;
-        inset: 0;
-        background-image: url(https://www.loliapi.com/acg/);
-        background-size: cover;
-        background-attachment: fixed;
-        background-position: center center;
-        pointer-events: none;
-    }
+    padding: .8rem;
+    background: black;
+    width: 100%;
+    height: 100%;
+    max-width: 600px;
+    margin: auto;
+    overflow: hidden;
+    display: grid;
+    position: relative;
 
-    .navi-card {
-        background-color: rgba(var(--v-theme-background), 0.4);
-        backdrop-filter: blur(3px);
-        transition: all 0.3s;
+    grid-template-columns: 1fr .7fr .3fr 1fr;
+    grid-template-rows: 20% 40% 20% 20%;
+    grid-template-areas:
+        'one two two three'
+        'four five five five'
+        'six five five five'
+        'six seven eight eight';
 
-        :deep(img) {
-            transform: translateY(100%);
-            transition: all 0.3s;
-        }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: 70% 20%;
+        transition: 0.3s;
+        cursor: pointer;
 
         &:hover {
-            color: #fff;
-            text-shadow: -1px -1px 1px #aaa, 2px 2px 1px #363636;
+            transform: scale(120%);
+        }
+    }
+}
 
-            :deep(img) {
-                transform: translate(0);
-            }
+
+// For clipping I used mozilla's inspect element. 
+.child {
+    position: absolute;
+    inset: 0;
+
+    &:nth-child(1),
+    &:nth-child(2),
+    &:nth-child(3) {
+        img {
+            width: 120%;
+            height: 120%;
+        }
+    }
+
+    &:first-child {
+        grid-area: one;
+        clip-path: polygon(0% 0%, 93.24% 0%, 105.04% 110.16%, 0% 90%);
+    }
+
+    &:nth-child(2) {
+        grid-area: two;
+        clip-path: polygon(0% 0%, 108.28% 0%, 96.45% 110.13%, 10.55% 110.93%);
+    }
+
+    &:nth-child(3) {
+        grid-area: three;
+        clip-path: polygon(15.05% 0%, 100% 0%, 99.35% 91.7%, 3.08% 108.48%);
+    }
+
+    &:nth-child(4) {
+        grid-area: four;
+        clip-path: polygon(0% -0.85%, 106.34% 9.98%, 121.32% 65.63%, 99.66% 109.89%, 1.86% 124.41%);
+
+        img {
+            width: 135%;
+            height: 135%;
+        }
+    }
+
+    &:nth-child(5) {
+        grid-area: five;
+        clip-path: polygon(6.4% 6.48%, 47.24% 5.89%, 100% 0%, 98.41% 96.85%, 53.37% 100%, 53% 63.21%, 3.23% 73.02%, 14.30% 44.04%);
+    }
+
+    &:nth-child(6) {
+        grid-area: six;
+        clip-path: polygon(2.14% 29.3%, 99.34% 15.42%, 98.14% 100.82%, 1.57% 101.2%);
+    }
+
+    &:nth-child(7) {
+        grid-area: seven;
+        clip-path: polygon(7.92% 33.47%, 96.31% 23.39%, 95.38% 100%, 5.30% 100.85%);
+    }
+
+    &:nth-child(8) {
+        grid-area: eight;
+        clip-path: polygon(2.5% 22.35%, 100% 0%, 100% 100%, 1.55% 100%);
+    }
+
+    &:nth-child(9) {
+        grid-row-start: 3;
+        grid-row-end: 4;
+        grid-column-start: 2;
+        grid-column-end: 4;
+
+        clip-path: polygon(5.94% 28.66%, 100.61% -0.67%, 101.1% 108.57%, 5.4% 126.28%);
+
+        img {
+            object-position: 30% 50%;
+            height: 135%;
         }
     }
 }
